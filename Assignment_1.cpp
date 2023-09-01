bc11e8f735bbb2718a35d97595128cc640adee4e
Aman Chauhan <aman23015@iiitd.ac.in>
2023-09-02 01:25:06 +0530

    Assignment 1 OOPD

--- Assignment_1.cpp
+++ Assignment_1.cpp
@@ -16,9 +16,66 @@ class read_input {
         float final_amount;
 
         void read_file(string);
-        float final(int input_amount, int);
-        float final_in_year(int input_amount,int yr);
+        float final(int , int);
+        float final_in_year(int ,int );
+        float final_spain(int,int);
+        float final_in_year_spain(int ,int );
+        void Check_value();
 };
+void read_input::Check_value(){
+    int peroid1 = 1977-1960;
+    float rate;
+    float SI;
+    float final_value=166;
+    for(int i=0;i<=peroid1;i++)
+    {
+        rate=interest_rate[i]-inflation[i];
+        SI = (final_value*rate)/100;
+        final_value = final_value + SI;
+        
+    }
+    float final_rate = (166 - final_value)/166;
+    cout<<"\nGrowth rate for Dictatorship period is :"<<final_rate<<endl;
+    
+    
+    final_value = 166;
+    int size2 = 1978 - 1960;
+    for(int i=size2;i<=size;i++)
+    {
+        if(i==(2002-1960))
+            final_value = final_value/166;
+        rate = interest_rate[i]-inflation[i];
+        SI = (final_value*rate)/100;
+        final_value = final_value + SI;
+    }
+    //cout<<p_value<<endl;
+    float final_rate_1 = (166/166 - final_value)/(166/166);
+    cout<<"Growth rate for Republican Era is :"<<final_rate_1<<endl;
+    if(final_rate > final_rate_1)
+        cout<<"The Growth rate for Dictatorship is more compared to Republican Era";
+    else
+        cout<<"The Growth rate for Republican Era is more compared to Dictatorship";
+}
+float read_input::final_in_year_spain(int input_amount,int yr){
+    float final_amount= input_amount;
+    float rate=interest_rate[yr-1960]-inflation[yr-1960];
+    float SI=(rate*final_amount)/100;
+    final_amount+=SI;
+    return final_amount;
+}
+float read_input::final_spain(int input_amount,int yr){
+    final_amount=input_amount;
+    float rate,SI;
+    for(int i=(yr-1960);i<=size;i++){
+        if(i==2002-1996){
+            final_amount=final_amount/166;
+        }
+        rate=interest_rate[i]-inflation[i];
+        SI=(rate*final_amount)/100;
+        final_amount=final_amount+SI;
+    }
+    return final_amount;
+}
 
 float read_input::final_in_year(int input_amount,int yr){
     float final_amount= input_amount;
@@ -30,11 +87,11 @@ float read_input::final_in_year(int input_amount,int yr){
 float read_input::final(int input_amount, int input_year){
     final_amount=input_amount;
     float rate,SI;
-    for(int i=input_year-1960;i<=size;i++){
+    for(int i=(input_year-1960);i<=size;i++){
         rate=interest_rate[i]-inflation[i];
         SI=(rate*final_amount)/100;
         final_amount=final_amount + SI;
-    }
+    }     
     return final_amount;
 }
 
@@ -77,7 +134,27 @@ int main(){
     cin>>input_yr_1;
     float final_Yr=obj.final_in_year(input_amount_1, input_yr_1);
     cout<<endl<<"Money after the year is : "<<final_Yr<<endl;
+
+    cout<<"Question_2 :"<<endl;
+    obj.read_file("spain_data.csv");
     
+    int input_amount_2,input_yr_2;
+    cout<<"Enter the amount of Investment in Spain bank : ";
+    cin>>input_amount_2;
+    cout<<endl<<"Enter the year : ";
+    cin>>input_yr_2;
+    cout<<"The total money received in spain bank is : ";
+    float final_amount_reseived_1=obj.final_spain(input_amount_2,input_yr_2);
+    cout<<final_amount_reseived_1<<endl;
+
+    int input_amount_3,input_yr_3;
+    cout<<"Enter the amount of Investment in spain bank to get the money after that year it was invested : ";
+    cin>>input_amount_3;
+    cout<<endl<<"Enter the year";
+    cin>>input_yr_3;
+    cout<<"Money after the year is :";
+    float final_am=obj.final_in_year_spain(input_amount_3,input_yr_3);
+    cout<<final_am<<endl;   
     return 0;
 }
 
